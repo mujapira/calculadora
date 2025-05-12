e65b2328a932f7b127be17376f24102f140f9e81
Mujapira <malmasmello2@gmail.com>
2025-05-12 20:33:23 -0300

    funcoes

--- lib/home.dart
+++ lib/home.dart
@@ -11,5 +11,44 @@ class CalculadoraHomePage extends StatefulWidget {
 class _CalculadoraHomePageState extends State<CalculadoraHomePage> {
   String display = "0";
   String historico = "";
-  String resultado = "0";
+  bool abreParenteses = true;
+
+  void atualizarDisplay(String valor) {
+    setState(() {
+      if (display == "0" && valor != ".") {
+        display = valor;
+      } else {
+        display += valor;
+      }
+    });
+  }
+
+  void aplicarPorcentagem() {
+    setState(() {
+      try {
+        final numero = double.parse(display);
+        setState(() {
+          display = (numero / 100).toString();
+        });
+      } catch (_) {}
+    });
+  }
+
+  void alternarSinal() {
+    setState(() {
+      if (display.startsWith('-')) {
+        display = display.substring(1);
+      } else if (display != "0") {
+        display = "-$display";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Calculadora')),
+      body: Center(child: Text(display, style: const TextStyle(fontSize: 48))),
+    );
+  }
 }
