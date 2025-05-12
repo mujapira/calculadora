a89937710e265b75f854445e59b083d50c7db57f
Mujapira <malmasmello2@gmail.com>
2025-05-12 20:03:28 -0300

    toma

--- lib/home.dart
+++ lib/home.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+
+class CalculadoraHomePage extends StatefulWidget {
+  const CalculadoraHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<CalculadoraHomePage> createState() => _CalculadoraHomePageState();
+}
